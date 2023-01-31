a94ac1057d20186ba49a205628ca3b498e5ca59e
zombeez90 <98763350+zombeez90@users.noreply.github.com>
2023-01-30 20:30:45 -0600

    add code

--- J/J/src/App.java
+++ J/J/src/App.java
@@ -1,5 +1,11 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -12,11 +18,32 @@ public class App {
 
         driver.get("https://opensource-demo.orangehrmlive.com/");
       //  driver.wait(5);
-        Thread.sleep(5000);;
+        Thread.sleep(5000);
 
+        
+        WebElement username = driver.findElement(By.cssSelector("[name = 'username']"));
+        username.sendKeys("Admin");
 
+        WebElement password = driver.findElement(By.cssSelector("[name = 'password']"));
+        password.sendKeys("admin123");
+
+        WebElement login = driver.findElement(By.cssSelector("[type = 'submit']"));
+        login.click();
+        Thread.sleep(3000);
+
+        driver.navigate().back();
+        Thread.sleep(3000);
+
+        username.sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
+        password.sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
+        Thread.sleep(1000);
+
+        WebElement forgot = driver.findElement(By.cssSelector("[class = 'oxd-text oxd-text--p orangehrm-login-forgot-header']"));
+        forgot.click();
+        Thread.sleep(3000);
 
 
         driver.close();
+      
     }
 }
